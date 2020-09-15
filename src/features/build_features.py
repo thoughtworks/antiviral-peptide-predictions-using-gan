aeaaac8bd8f514afb33c32eeab0eed0f839263e7
Shraddha Surana <shraddha.surana@thoughtworks.com>
2020-09-15 15:25:35 +0530

    creating function to plot for avp, non-avp and generated data

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -6,7 +6,6 @@ import numpy as np
 
 import os
 
-os.chdir("/Users/shraddhasurana/Desktop/projects/E4R/LifeSciences/ddh/antiviral-peptide-predictions-using-gan/src")
 
 def create_sequence_properties_dataframe(sequences):
     params = ['seq', 'aa_counts', 'aa_percentages', 'molecular_weight', 'aromaticity', 'instability_index',
@@ -41,62 +40,105 @@ def create_sequence_properties_dataframe(sequences):
         seq_properties = seq_properties.append(row)
     return seq_properties
 
-sequences = pd.read_csv('../data/raw/AVP_data.csv')
-avp_seq_properties = create_sequence_properties_dataframe(sequences)
-avp_seq_properties['activity'] = 'AVP'
-non_avp_seq_properties = create_sequence_properties_dataframe(pd.read_csv('../data/raw/non_AVP_data.csv'))
-non_avp_seq_properties['activity'] = 'non-AVP'
-
-all_data = avp_seq_properties.append(non_avp_seq_properties, ignore_index = True)
-
-
 # ---- Visualizations on seq properties ----
 
-def create_distributions(amp_data, non_amp_data, properties):
+def create_distributions(amp_data, non_amp_data, generated_avp_data, properties, save=False):
     kwargs = dict(hist_kws={'alpha': .3}, kde_kws={'linewidth': 4}, bins=200)
     for property in properties:
         print(property)
         plt.figure(figsize=(10, 7), dpi=80)
-        sns.distplot(amp_data[property], color="g", label="Real_AMP", **kwargs)
-        sns.distplot(non_amp_data[property], color="y", label="non-AMP", **kwargs)
+        sns.distplot(amp_data[property], color="g", label="Real_AVP", **kwargs)
+        sns.distplot(non_amp_data[property], color="y", label="non-AVP", **kwargs)
+        sns.distplot(generated_avp_data[property], color="m", label="generated-AVP", **kwargs)
         plt.legend()
         plt.title(property)
-        plt.savefig("../reports/figures/distribution_"+property+".png")
+        if save:
+            plt.savefig("../reports/figures/distribution_"+property+".png")
         plt.show()
 
 
-def create_box_plots(data, properties):
+def create_box_plots(data, properties, save=False):
     for property in properties:
         print(property)
         plt.figure(figsize=(10, 7), dpi=80)
         ax = sns.boxenplot(x="activity", y=property, hue="activity", data=data, palette="Set3")
         plt.legend()
         plt.title(property)
-        plt.savefig("../reports/figures/box_plot_"+property+".png")
+        if save:
+            plt.savefig("../reports/figures/box_plot_"+property+".png")
         plt.show()
 
 
-def create_iqr_hist(amp_data, non_amp_data, properties):
+def create_iqr_hist(positive_data, negative_data, generated_data, properties, save=False):
     kwargs = dict(hist_kws={'alpha': .3}, kde_kws={'linewidth': 4}, bins=100)
     for property in properties:
         print(property)
         plt.figure(figsize=(10, 7), dpi=80)
-        sns.distplot(amp_data[property], hist=True, color="g", label="Real_AMP", **kwargs)
-        sns.distplot(non_amp_data[property], hist=True, color="y", label="non-AMP", **kwargs)
+        sns.distplot(positive_data[property], hist=True, color="g", label="Real_AMP", **kwargs)
+        sns.distplot(negative_data[property], hist=True, color="y", label="non-AMP", **kwargs)
+        sns.distplot(generated_data[property], hist=True, color="m", label="non-AMP", **kwargs)
 
-        Q1_amp = np.percentile(amp_data[property], 25, interpolation='midpoint')
-        Q3_amp = np.percentile(amp_data[property], 75, interpolation='midpoint')
-        Q1_non_amp = np.percentile(non_amp_data[property], 25, interpolation='midpoint')
-        Q3_non_amp = np.percentile(non_amp_data[property], 75, interpolation='midpoint')
+        Q1_amp = np.percentile(positive_data[property], 25, interpolation='midpoint')
+        Q3_amp = np.percentile(positive_data[property], 75, interpolation='midpoint')
+        Q1_non_amp = np.percentile(negative_data[property], 25, interpolation='midpoint')
+        Q3_non_amp = np.percentile(negative_data[property], 75, interpolation='midpoint')
+        Q1_generated_amp = np.percentile(generated_data[property], 25, interpolation='midpoint')
+        Q3_generated_amp = np.percentile(generated_data[property], 75, interpolation='midpoint')
 
-        plt.xlim([min(Q1_amp,Q1_non_amp), max(Q3_amp, Q3_non_amp)])
+        plt.xlim([min(Q1_amp,Q1_non_amp,Q1_generated_amp), max(Q3_amp, Q3_non_amp,Q3_generated_amp)])
         plt.legend()
         plt.title(property)
-        plt.savefig("../reports/figures/iqr_hist_" + property + ".png")
+        if save:
+            plt.savefig("../reports/figures/iqr_hist_" + property + ".png")
         plt.show()
 
+def create_properties_and_plots(avp_data_path, non_avp_data_path, generated_avp_data_path, save_plots=False):
+    """
+    params:
+    All data must have sequences under the header 'Sequence'
+    """
+    sequences = pd.read_csv(avp_data_path)
+    avp_seq_properties = create_sequence_properties_dataframe(sequences)
+    avp_seq_properties['activity'] = 'AVP'
 
-properties_to_plot = ['molecular_weight', 'aromaticity', 'instability_index', 'isoelectric_point', 'helix', 'turn', 'sheet', 'with_reduced_cysteines', 'with_disulfid_bridges', 'gravy']
-create_distributions(avp_seq_properties, non_avp_seq_properties, properties_to_plot)
-create_box_plots(all_data, properties_to_plot)
-create_iqr_hist(avp_seq_properties, non_avp_seq_properties, properties_to_plot)
+    sequences = pd.read_csv(non_avp_data_path)
+    non_avp_seq_properties = create_sequence_properties_dataframe(sequences)
+    non_avp_seq_properties['activity'] = 'non-AVP'
+
+    sequences = pd.read_csv(generated_avp_data_path)
+    generated_avp_seq_properties = create_sequence_properties_dataframe(sequences)
+    generated_avp_seq_properties['activity'] = 'generated-AVP'
+
+    all_data_temp = avp_seq_properties.append(non_avp_seq_properties, ignore_index=True)
+    all_data = all_data_temp.append(generated_avp_seq_properties, ignore_index=True)
+    del all_data_temp
+
+    properties_to_plot = ['molecular_weight', 'aromaticity', 'instability_index', 'isoelectric_point', 'helix', 'turn','sheet', 'with_reduced_cysteines', 'with_disulfid_bridges', 'gravy']
+    create_distributions(avp_seq_properties, non_avp_seq_properties, generated_avp_seq_properties, properties_to_plot, save_plots)
+    properties_for_box_plot = ['molecular_weight', 'aromaticity', 'instability_index', 'isoelectric_point', 'helix', 'turn', 'sheet', 'gravy']
+    create_box_plots(all_data, properties_for_box_plot, save_plots)
+    create_iqr_hist(avp_seq_properties, non_avp_seq_properties, generated_avp_seq_properties, properties_to_plot, save_plots)
+
+
+
+if __name__ == '__main__':
+    os.chdir("/Users/shraddhasurana/Desktop/projects/E4R/LifeSciences/ddh/antiviral-peptide-predictions-using-gan/src")
+
+    avp_sequences = pd.read_csv('../data/raw/AVP_data.csv')
+    avp_seq_properties = create_sequence_properties_dataframe(avp_sequences)
+    avp_seq_properties['activity'] = 'AVP'
+    non_avp_seq_properties = create_sequence_properties_dataframe(pd.read_csv('../data/raw/non_AVP_data.csv'))
+    non_avp_seq_properties['activity'] = 'non-AVP'
+
+    generated_avp_seq_properties = create_sequence_properties_dataframe(pd.read_csv('../data/generated/generated_AVP_data.csv'))
+    generated_avp_seq_properties['activity'] = 'generated-AVP'
+
+    all_data_temp = avp_seq_properties.append(non_avp_seq_properties, ignore_index=True)
+    all_data = all_data_temp.append(generated_avp_seq_properties, ignore_index=True)
+    del all_data_temp
+
+    properties_to_plot = ['molecular_weight', 'aromaticity', 'instability_index', 'isoelectric_point', 'helix', 'turn', 'sheet', 'with_reduced_cysteines', 'with_disulfid_bridges', 'gravy']
+    create_distributions(avp_seq_properties, non_avp_seq_properties, generated_avp_seq_properties, properties_to_plot)
+    properties_for_box_plot = ['molecular_weight', 'aromaticity', 'instability_index', 'isoelectric_point', 'helix', 'turn', 'sheet', 'gravy']
+    create_box_plots(all_data, properties_for_box_plot)
+    create_iqr_hist(avp_seq_properties, non_avp_seq_properties, generated_avp_seq_properties, properties_to_plot)
