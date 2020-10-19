42602834c101c0676d7da3e915414508a2e333e5
Shraddha Surana <shraddha.surana@thoughtworks.com>
2020-10-19 11:13:28 +0530

    changes in epocs in maliGAN

--- MaliGAN/models/Gan.py
+++ MaliGAN/models/Gan.py
@@ -14,8 +14,8 @@ class Gan:
         self.sess = init_sess()
         self.metrics = list()
         self.epoch = 0
-        self.pre_epoch_num = 1 #Changed parameter
-        self.adversarial_epoch_num = 2 #changed parameter
+        self.pre_epoch_num = 100 #Changed parameter
+        self.adversarial_epoch_num = 1000 #changed parameter
         self.log = None
         self.reward = None
 
