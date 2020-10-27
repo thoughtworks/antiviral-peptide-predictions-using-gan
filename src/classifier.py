94acf0fc37516ad186508736fc6bd0c9d0f9b4bc
Shraddha Surana <shraddha.surana@thoughtworks.com>
2020-10-27 15:15:34 +0530

    adding AA for decision tree classifier

--- src/classifier.py
+++ src/classifier.py
@@ -3,14 +3,21 @@ import os
 
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import confusion_matrix
-from sklearn import tree, metrics
+from sklearn import tree
 import matplotlib.pyplot as plt
 
+from src.evaluate import evaluate_classifier
 from src.features.build_features import create_sequence_properties_dataframe
 
 os.getcwd()
 os.chdir('/Users/shraddhasurana/Desktop/projects/E4R/LifeSciences/ddh/antiviral-peptide-predictions-using-gan')
+
+"""
+Datasets:
+highly active - manually curated : data/raw/manual_lowMIC.csv
+avp - low_mic_90perc : AVPpred_Low_MIC_data_filtered_90perc.csv
+nonAVP_AMP - 90perc : raw/AMP_nonAVP_filtered_negative.csv
+"""
 
 avp = pd.read_csv("data/raw/manual_lowMIC.csv")
 non_avp = pd.read_csv("data/raw/AMP_nonAVP_filtered_negative.csv")
@@ -21,26 +28,30 @@ non_avp_seq_properties = create_sequence_properties_dataframe(non_avp)
 avp_seq_properties['Activity'] = 1
 non_avp_seq_properties['Activity'] = 0
 
+avp_seq_properties = pd.concat([avp_seq_properties.drop(['aa_percentages'], axis=1), avp_seq_properties['aa_percentages'].apply(pd.Series)], axis=1)
+non_avp_seq_properties = pd.concat([non_avp_seq_properties.drop(['aa_percentages'], axis=1), non_avp_seq_properties['aa_percentages'].apply(pd.Series)], axis=1)
+
 data = avp_seq_properties
 data = data.append(non_avp_seq_properties)
-params = ['molecular_weight', 'aromaticity', 'instability_index', 'isoelectric_point', 'helix', 'turn', 'sheet', 'gravy', 'net_charge_at_pH7point4']
+params = ['molecular_weight', 'aromaticity', 'instability_index', 'isoelectric_point', 'helix', 'turn', 'sheet', 'gravy', 'net_charge_at_pH7point4','A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']
 # Creating a DT classifier
 
 X_train, X_test, Y_train, Y_test = train_test_split(data[params], data['Activity'], random_state=0)
 
-clf = DecisionTreeClassifier(max_depth = 5, random_state = 0, criterion="entropy")
+# ---- Standarise the data ----
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+scaler.fit(X_train)
+
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
+# ---- Decision Tree classifier ----
+clf = DecisionTreeClassifier(max_depth = 50, random_state = 0, criterion="entropy")
 clf.fit(X_train, Y_train)
 predictions = clf.predict(X_test)
-print(confusion_matrix(Y_test,predictions))
-# metrics.accuracy_score(Y_test,predictions)
-metrics.balanced_accuracy_score(Y_test,predictions)
+evaluate_classifier(Y_test, predictions)
 
 fig, axes = plt.subplots(nrows = 1,ncols = 1,figsize = (10,2), dpi=3000)
 tree.plot_tree(clf, feature_names=params, class_names=['0','1'],filled=True)
-fig.savefig('DT_depth5.png')
-
-"""
-highly active - manually curated - data/raw/manual_lowMIC.csv
-avp - low_mic_90perc - AVPpred_Low_MIC_data_filtered_90perc.csv
-nonAVP_AMP - 90perc - raw/AMP_nonAVP_filtered_negative.csv
-"""
+fig.savefig('DT_depth50_withAA.png')
---
+++ src/evaluate.py
@@ -0,0 +1,28 @@
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix
+
+from src.features.build_features import create_properties_and_plots, create_sequence_properties_dataframe
+import pandas as pd
+
+
+def evaluated(sequences):
+    natural_avp = pd.read_csv("")
+    seq_properties = create_sequence_properties_dataframe(natural_avp)
+    create_properties_and_plots('metadata.csv', '../../reports/')
+    pass
+
+
+
+def evaluate_classifier(actual, predicted):
+    print("---- Confusion matrix ----")
+    print(confusion_matrix(actual, predicted))
+    # metrics.accuracy_score(Y_test,predictions)
+
+    print("---- Balanced accuracy score ----")
+    print(metrics.balanced_accuracy_score(actual, predicted))
+
+    print("---- F1 score ----")
+    print(metrics.f1_score(actual, predicted))
+
+    print("---- Classification report ----")
+    print(metrics.classification_report(actual, predicted))
